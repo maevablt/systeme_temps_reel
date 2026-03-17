3206a77b7cf2fd6c10c18efd8d1a8576ac1c4d40
maevablt <maeva.bolteau@ipsa.fr>
2026-03-17 08:33:25 +0100

    Add files via upload

---
+++ TP1.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "Header_TP1.h"
+
+/*int main() {
+    printf("Hello World\n");
+    return 0 ;
+}
+*/
+
+/*int main() {
+    int x = 20;
+    int y = 10;
+    
+    //comparer(x, y);
+    //compterfor(100);
+    //number_adress(10);
+}*/
+int main() {
+    int liste[] = {10, 25, 42, 88, 97};
+    int taille = sizeof(liste) / sizeof(liste[0]); sizeof(liste); // Donne la taille totale du tableau en octets /Donne la taille d'un seul élément
+    int cible = 42;
+
+    int resultat = rechercher_valeur(liste, taille, cible);
+
+    if (resultat != -1) {
+        printf("Valeur %d trouvee a l'index %d\n", cible, resultat);
+    } else {
+        printf("Valeur non trouvee.\n");
+    }
+
+    return 0;
+}
